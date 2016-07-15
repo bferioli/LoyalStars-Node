6bc253cf07027daa489f76647d5c3136f6698f0b
Brandon Ferioli <bferioli@gmail.com>
2016-07-15 18:27:29 -0400

    Add dotenv environment config

--- helpers/phone.js
+++ helpers/phone.js
@@ -2,9 +2,6 @@ var helpers = {};
 var bases = require('bases');
 var Q = require('q');
 var twilio = require('twilio');
-var TWILIO_ACCOUNT_SID = 'AC0050f813954db69b3135e0b2ffec610e';
-var TWILIO_AUTH_TOKEN = '81dc7c26ee78c156b15a3a53e71c5d3a';
-var TWILIO_NUMBER = '+19292388238';
 
 helpers.encodePhone = function(phone) {
     return bases.toBase62(phone);
@@ -15,13 +12,13 @@ helpers.decodePhone = function(encoded) {
 };
 
 helpers.sendRewardMessage = function(data, phone) {
-    var client = new twilio.RestClient(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
+    var client = new twilio.RestClient(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
     var deferred = Q.defer();
 
     client.sms.messages.create({
         to: '+1' + phone,
-        from: TWILIO_NUMBER,
-        body: "You've earned a reward at " + data.location.name + ", to claim visit http://4654b033.ngrok.io/reward/" + data.reward._id + "/" + phone
+        from: process.env.TWILIO_NUMBER,
+        body: "You've earned a reward at " + data.location.name + ", to claim visit " + process.env.URL_PATH + "/reward/" + data.reward._id + "/" + phone
     }, function(error, message){
         if (error) {
             deferred.reject(new Error(error));
