190202029f2b86bfa493b51d8dac4e9d7574d9d7
Brandon Ferioli <bferioli@gmail.com>
2017-07-25 18:33:00 -0400

    Initial React app configuration

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(flash());
 
-//  Connect all our routes to our application
+// Connect all our routes to our application
 app.use('/', routes(app, passport));
 
 // catch 404 and forward to error handler
--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,18 @@ module.exports = (app, passport) => {
     const api = require('./api');
     const dashboard = require('./dashboard');
     const twilio = require('./twilio');
+    const path = require('path');
 
     routes.use('/api', api(app));
     routes.use('/dashboard', dashboard(app, passport));
     routes.use('/twilio', twilio(app));
+
+    if (app.get('env') === 'production') {
+        // Direct all unhandled requests to React client
+        routes.get('*', (req, res) => {
+            res.sendFile(path.join(__dirname + '/../client/build/index.html'));
+        });
+    }
 
     return routes;
 };
